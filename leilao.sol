3ca4fcc88591ad597bc8ff834b3426b0663d5fe1
Jeff Prestes <jeffprestes@gmail.com>
2019-05-07 19:34:36 -0300

    Create leilao.sol

---
+++ leilao.sol
@@ -0,0 +1,65 @@
+pragma solidity 0.5.8;
+
+contract Leilao {
+
+    address payable public contaGovernamental;
+    uint public prazoFinalLeilao;
+
+    address public maiorOfertante;
+    uint public maiorLance;
+
+    mapping(address => uint) public lancesRealizados;
+
+    bool public encerrado;
+
+    event novoMaiorLance(address ofertante, uint valor);
+    event fimDoLeilao(address arrematante, uint valor);
+
+    modifier somenteGoverno {
+        require(msg.sender == contaGovernamental, "Somente Governo pode realizar essa operacao");
+        _;
+    }
+
+    constructor(
+        uint _duracaoLeilao,
+        address payable _contaGovernamental
+    ) public {
+        contaGovernamental = _contaGovernamental;
+        prazoFinalLeilao = now + _duracaoLeilao;
+    }
+
+
+    function lance() public payable {
+ 
+        require(
+            now <= prazoFinalLeilao,
+            "Leilao encerrado."
+        );
+
+        require(
+            msg.value > maiorLance,
+            "Ja foram apresentados lances maiores."
+        );
+        
+        maiorOfertante = msg.sender;
+        maiorLance = msg.value;
+         
+        if (maiorLance != 0) {
+            lancesRealizados[maiorOfertante] += maiorLance;
+        }
+        
+        emit novoMaiorLance (msg.sender, msg.value);
+    }
+
+   
+    function finalizaLeilao() public somenteGoverno {
+       
+        require(now >= prazoFinalLeilao, "Leilao ainda nao encerrado.");
+        require(!encerrado, "Leilao encerrado.");
+
+        encerrado = true;
+        emit fimDoLeilao(maiorOfertante, maiorLance);
+
+        contaGovernamental.transfer(address(this).balance);
+    }
+}

21e0d614662ef690440664ea673c609a141457ad
Jeff Prestes <jeffprestes@gmail.com>
2019-10-08 22:59:41 -0300

    for e require

--- aluguelsemcomentarios.sol
+++ aluguelsemcomentarios.sol
@@ -6,9 +6,10 @@ pragma solidity 0.5.12;
 
 contract Aluguel 
 {
-    string            public                       locatario;
+    string public locatario;
     string public locador;
     uint256 private valor;
+    uint256 constant public numeroMaximoLegalDeAlgueisParaMulta = 3;
 
     constructor(string memory nomeLocador, string memory nomeLocatario, uint256 valorDoAluguel) public 
     {
@@ -22,18 +23,35 @@ contract Aluguel
         return valor;
     }
  
-    function simulaMulta( uint256 mesesRestantes, uint256 totalMesesContato) public view returns(uint256 valorMulta) 
+    function simulaMulta( uint256 mesesRestantes, uint256 totalMesesContrato) public view returns(uint256 valorMulta) 
     {
         valorMulta = valor*numeroMaximoLegalDeAlgueisParaMulta;
-        valorMulta = valorMulta/totalMesesContato;
+        valorMulta = valorMulta/totalMesesContrato;
         valorMulta = valorMulta*mesesRestantes;
         return valorMulta;
     } 
         
     function reajustaAluguel(uint256 percentualReajuste) public 
     {
+        if (percentualReajuste > 20) 
+        {
+            percentualReajuste = 20;
+        }
         uint256 valorDoAcrescimo = 0;
         valorDoAcrescimo = ((valor*percentualReajuste)/100);
         valor = valor + valorDoAcrescimo;
+    }
+    
+    function aditamentoValorAluguel(uint256 valorCerto) public
+    {
+        valor = valorCerto;
+    }
+
+    function aplicaMulta(uint256 mesesRestantes, uint256 percentual) public
+    {
+        require(mesesRestantes<30, "Periodo de contrato inválido");
+        for (uint i=1; i<mesesRestantes; i++) {
+            valor = valor+((valor*percentual)/100);
+        }
     }
 }

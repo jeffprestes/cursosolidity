0503d226c18362dcb6dc21903633c605122d13ec
pedroterradias <56138102+pedroterradias@users.noreply.github.com>
2019-10-15 18:58:48 -0300

    Update CompraeVendaDelivery.sol

--- CompraeVendaDelivery.sol
+++ CompraeVendaDelivery.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.5.12;
 
-contract Rappi
+contract rappi
 {
     string public vendedor;
     string public comprador;
@@ -10,38 +10,38 @@ contract Rappi
     bool[] public statusPagamento;
     address payable public contaVendedor;
 
-constructor(string memory nomevendedor, string memory nomecomprador, uint256 valorObjeto, uint256 distanciaEntrega, address payable paramContaVendedor) public    
-{    vendedor = nomevendedor;
+    constructor(string memory nomevendedor, string memory nomecomprador, uint256 valorObjeto, uint256 distanciaEntrega, address payable paramContaVendedor) public    
+    {
+    vendedor = nomevendedor;
     comprador = nomecomprador;     
     distancia = distanciaEntrega;
     valordoProduto = valorObjeto;
     contaVendedor = paramContaVendedor;
-}
-
-function valordaEntrega (uint) public view returns (uint valordaEntrega)
-{
-    if (distancia> 5)
-    {
-        require(distancia>4, "será cobrada taxa de entrega");
-        for (uint256 i=5; i<=distancia; i++)
-            valordaEntrega = distancia;
     }
-    else (distancia <= 5);
+
+    function valordaEntrega (uint) public view returns (uint valordaEntrega)
     {
-        valordaEntrega = valorEntregaGratis;
-        
+        if (distancia> 5)
+        {
+            require(distancia>4, "será cobrada taxa de entrega");
+            for (uint256 i=5; i<=distancia; i++)
+                valordaEntrega = distancia;
+        }
+        else (distancia <= 5);
+        {
+            valordaEntrega = valorEntregaGratis;
+        }
     }
-}    
-
-function valorTotalCompra (uint256 valordaEntrega) public view returns (uint valorTotaldaCompra)
-{
-    valorTotaldaCompra = valordaEntrega + valordoProduto;
-}
-
-
-function receberPagamento() public payable
-{
-    require(msg.value>= uint valorTotaldaCompra, "Pagamento da Compra");
-    contaVendedor.transfer(msg.value);
-    statusPagamento.push(true);
+    
+    function valorTotalCompra (uint256 valordaEntrega) public view returns (uint valorTotaldaCompra)
+    {
+        valorTotaldaCompra = valordaEntrega + valordoProduto;
+    }
+    
+    function receberPagamento() public payable
+    {
+       require(msg.value>= uint valorTotaldaCompra, "Pagamento da Compra");
+       contaVendedor.transfer(msg.value);
+       statusPagamento.push(true);
+    }
 }

5bc7dad105776cbd2a29df5714604239d1611f9f
Jeff Prestes <jeffprestes@gmail.com>
2020-06-23 11:32:56 -0300

    update to new solidity version

--- tokenerc20.sol
+++ tokenerc20.sol
@@ -1,17 +1,79 @@
-pragma solidity 0.5.12;
+// SPDX-License-Identifier: MIT
 
-contract ERC20Interface {
-    function totalSupply() public view returns(uint amount);
-    function balanceOf(address tokenOwner) public view returns(uint balance);
-    function allowance(address tokenOwner, address spender) public view returns(uint balanceRemaining);
-    function transfer(address to, uint tokens) public returns(bool status);
-    function approve(address spender, uint limit) public returns(bool status);
-    function transferFrom(address from, address to, uint amount) public returns(bool status);
-    function name() public view returns(string memory tokenName);
-    function symbol() public view returns(string memory tokenSymbol);
+pragma solidity ^0.6.8;
 
-    event Transfer(address from, address to, uint amount);
-    event Approval(address tokenOwner, address spender, uint amount);
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
 contract Owned {
@@ -34,65 +96,71 @@ contract Mortal is Owned  {
     }
 }
 
-contract TicketERC20 is ERC20Interface, Mortal {
+contract TicketERC20 is IERC20, Mortal {
     string private myName;
     string private mySymbol;
-    uint private myTotalSupply;
-    uint8 public decimals;
+    uint256 private myTotalSupply;
+    uint256 public decimals;
 
-    mapping (address=>uint) balances;
-    mapping (address=>mapping (address=>uint)) ownerAllowances;
+    mapping (address=>uint256) balances;
+    mapping (address=>mapping (address=>uint256)) ownerAllowances;
 
     constructor() public {
-        myName = "EPD Creditos";
-        mySymbol = "EPDC2019";
-        myTotalSupply = 1000000;
-        decimals = 0;
-        balances[msg.sender] = myTotalSupply;
+        myName = "EPD Creditos - 23 06 2020 11 30";
+        mySymbol = "EPDC202006231130";
+        decimals = 2;
+        _mint(msg.sender, (100000000 * (10 ** decimals)));
     }
 
-    function name() public view returns(string memory tokenName) {
+    function name() public view returns(string memory) {
         return myName;
     }
 
-    function symbol() public view returns(string memory tokenSymbol) {
+    function symbol() public view returns(string memory) {
         return mySymbol;
     }
 
-    function totalSupply() public view returns(uint amount) {
+    function totalSupply() public override view returns(uint256) {
         return myTotalSupply;
     }
 
-    function balanceOf(address tokenOwner) public view returns(uint balance) {
-        require(tokenOwner != address(0));
+    function balanceOf(address tokenOwner) public override view returns(uint256) {
         return balances[tokenOwner];
     }
 
-    function allowance(address tokenOwner, address spender) public view returns(uint balanceRemaining) {
+    function allowance(address tokenOwner, address spender) public override view returns(uint256) {
         return ownerAllowances[tokenOwner][spender];
     }
 
-    function transfer(address to, uint amount) public hasEnoughBalance(msg.sender, amount) tokenAmountValid(amount) returns(bool status) {
+    function transfer(address to, uint256 amount) public override  hasEnoughBalance(msg.sender, amount) tokenAmountValid(amount) returns(bool) {
         balances[msg.sender] -= amount;
         balances[to] += amount;
         emit Transfer(msg.sender, to, amount);
         return true;
     }
 
-    function approve(address spender, uint limit) public returns(bool status) {
+    function approve(address spender, uint limit) public override returns(bool) {
         ownerAllowances[msg.sender][spender] = limit;
         emit Approval(msg.sender, spender, limit);
         return true;
     }
 
-    function transferFrom(address from, address to, uint amount) public 
+    function transferFrom(address from, address to, uint256 amount) public override 
     hasEnoughBalance(from, amount) isAllowed(msg.sender, from, amount) tokenAmountValid(amount)
-    returns(bool status) {
+    returns(bool) {
         balances[from] -= amount;
         balances[to] += amount;
         ownerAllowances[from][msg.sender] = amount;
         emit Transfer(from, to, amount);
         return true;
+    }
+    
+    function _mint(address account, uint256 amount) internal virtual {
+        require(account != address(0), "ERC20: mint to the zero address");
+
+        myTotalSupply = myTotalSupply + amount;
+        balances[account] = balances[account] + amount;
+        emit Transfer(address(0), account, amount);
     }
 
     modifier hasEnoughBalance(address owner, uint amount) {

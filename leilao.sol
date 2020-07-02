caab485bc82cf74cfb1b2eba8ee5e3fa4a65899f
Jeff Prestes <jeffprestes@gmail.com>
2020-07-02 19:35:30 -0300

    update solidity version

--- leilao.sol
+++ leilao.sol
@@ -1,4 +1,9 @@
-pragma solidity 0.5.12;
+/*
+SPDX-License-Identifier: CC-BY-4.0
+(c) Desenvolvido por Jeff Prestes
+This work is licensed under a Creative Commons Attribution 4.0 International License.
+*/
+pragma solidity 0.6.10;
 
 contract Leilao {
 

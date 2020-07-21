ff6648f854b1eb04869e9fbeed64dae2058606c6
Jeff Prestes <jeffprestes@gmail.com>
2020-07-21 17:55:40 -0300

    update solidity version

--- registroImovel.sol
+++ registroImovel.sol
@@ -1,4 +1,9 @@
-pragma solidity 0.5.8;
+/*
+SPDX-License-Identifier: CC-BY-4.0
+(c) Desenvolvido por Jeff Prestes
+This work is licensed under a Creative Commons Attribution 4.0 International License.
+*/
+pragma solidity 0.6.11;
 
 contract RegistroImovel {
     

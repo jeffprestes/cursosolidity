f4340c1a6d6aa4782cd7e9169848f74a0bd25532
Jeff Prestes <jeffprestes@gmail.com>
2023-09-15 16:29:12 -0300

    Atualiza para a versao 0.8.19

--- aluguelsemcomentarios.sol
+++ aluguelsemcomentarios.sol
@@ -3,14 +3,13 @@ SPDX-License-Identifier: CC-BY-4.0
 (c) Desenvolvido por Jeff Prestes
 This work is licensed under a Creative Commons Attribution 4.0 International License.
 */
-pragma solidity 0.6.10;
+pragma solidity 0.8.19;
 
-contract Aluguel 
-{
+contract Aluguel {
     string public locatario;
     string public locador;
     uint256 private valor;
-    uint256 constant public numeroMaximoLegalDeAlgueisParaMulta = 3;
+    uint256 constant public numeroMaximoLegalDeAlugueisParaMulta = 3;
     bool[] public statusPagamento;
     /*
     0 - 01/2020 = true
@@ -18,24 +17,27 @@ contract Aluguel
     2 - 03/2020 = true
     3 - 04/2020 = true
     */
-    address payable public contaLocatario;
+    address payable public contaLocador;
+    address public owner;
 
-    constructor(string memory nomeLocador, string memory nomeLocatario, address payable paramContaLocatario, uint256 valorDoAluguel) public 
-    {
-        locador = nomeLocador;
-        locatario = nomeLocatario;
-        valor = valorDoAluguel;
-        contaLocatario = paramContaLocatario;
+    constructor(    string memory _nomeLocador, 
+                    string memory _nomeLocatario, 
+                    address payable _contaLocador, 
+                    uint256 _valorDoAluguel)  payable {
+        locador = _nomeLocador;
+        locatario = _nomeLocatario;
+        valor = _valorDoAluguel;
+        contaLocador = _contaLocador;
+        owner = msg.sender;
     }
  
-    function valorAtualDoAluguel() public view returns (uint256) 
-    {
+    function valorAtualDoAluguel() public view returns (uint256) {
         return valor;
     }
  
     function simulaMulta( uint256 mesesRestantes, uint256 totalMesesContrato) public view returns(uint256 valorMulta) 
     {
-        valorMulta = valor*numeroMaximoLegalDeAlgueisParaMulta;
+        valorMulta = valor*numeroMaximoLegalDeAlugueisParaMulta;
         valorMulta = valorMulta/totalMesesContrato;
         valorMulta = valorMulta*mesesRestantes;
         return valorMulta;
@@ -43,8 +45,7 @@ contract Aluguel
         
     function reajustaAluguel(uint256 percentualReajuste) public 
     {
-        if (percentualReajuste > 20) 
-        {
+        if (percentualReajuste > 20) {
             percentualReajuste = 20;
         }
         uint256 valorDoAcrescimo = 0;
@@ -52,14 +53,12 @@ contract Aluguel
         valor = valor + valorDoAcrescimo;
     }
     
-    function aditamentoValorAluguel(uint256 valorCerto) public
-    {
+    function aditamentoValorAluguel(uint256 valorCerto) public   {
         valor = valorCerto;
     }
 
-    function aplicaMulta(uint256 mesesRestantes, uint256 percentual) public
-    {
-        require(mesesRestantes<30, "Periodo de contrato inválido");
+    function aplicaMulta(uint256 mesesRestantes, uint256 percentual) public     {
+        require(mesesRestantes<30, "Periodo de contrato invalido");
         for (uint numeroDeVoltas=0; numeroDeVoltas < mesesRestantes; numeroDeVoltas=numeroDeVoltas+2) {
             valor = valor+((valor*percentual)/100);
         }
@@ -68,7 +67,7 @@ contract Aluguel
     
     function receberPagamento() public payable {
         require(msg.value>=valor, "Valor insuficiente");
-        contaLocatario.transfer(msg.value);
+        contaLocador.transfer(msg.value);
         statusPagamento.push(true);
     }
     
@@ -78,7 +77,7 @@ contract Aluguel
         if ((valor * _parametro) > 5000) {
             return "Muito caro";
         } else {
-            return "Preço razoavel";
+            return "Preco razoavel";
         }
     }
     

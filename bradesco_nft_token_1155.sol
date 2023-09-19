aacaece94a20dd7974b2d5adb0f430fce463254c
Jeff Prestes <jeffprestes@gmail.com>
2023-09-19 16:36:20 -0300

    Update bradesco_nft_token_1155.sol

--- bradesco_nft_token_1155.sol
+++ bradesco_nft_token_1155.sol
@@ -1,1 +1,58 @@
-aa
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+
+contract BradescoCDB is ERC1155, Ownable, Pausable, ERC1155Burnable, ERC1155Supply {
+    
+    string public tokenURI;    
+
+    //"https://jeffprestes.github.io/nft-bradesco/"
+    constructor(string memory _tokenURI) ERC1155(_tokenURI) {
+        tokenURI = _tokenURI;
+    }
+
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    //id = codigo de instrumento financeiro
+    function mint(address account, uint256 id, uint256 amount, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mint(account, id, amount, data);
+    }
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mintBatch(to, ids, amounts, data);
+    }
+
+    function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        internal
+        whenNotPaused
+        override(ERC1155, ERC1155Supply)
+    {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+    }
+
+    function uri(uint256 id) public view override returns (string memory) {
+        return string(abi.encodePacked(tokenURI, id, ".json"));
+    }
+
+}

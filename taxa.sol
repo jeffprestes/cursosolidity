4d605378ed72f13d5aaf0351bcecdca936d3fb22
Jeff Prestes <jeffprestes@gmail.com>
2023-09-20 16:40:28 -0300

    Update taxa.sol

--- taxa.sol
+++ taxa.sol
@@ -1,1 +1,12 @@
-aa
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+// Mumbai: 0xEed2fE736e53E65412A3c3b8326260218bD40c16
+contract Taxa {
+    uint256 public taxaBase = 1;
+    
+    function setTaxaBase(uint256 _novaTaxaBase) public returns (uint256) {
+        taxaBase = _novaTaxaBase;
+        return taxaBase;
+    }
+}

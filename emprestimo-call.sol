6174f2d650d10586ac98ab4470c16a3d5193db57
Jeff Prestes <jeffprestes@gmail.com>
2023-09-20 16:43:31 -0300

    Update emprestimo-call.sol

--- emprestimo-call.sol
+++ emprestimo-call.sol
@@ -1,1 +1,21 @@
-sss
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+contract EmprestimoCall {
+
+    address public addTaxa = 0xEed2fE736e53E65412A3c3b8326260218bD40c16;
+
+    function setTaxaComCall(uint256 _val) public returns (bytes memory) {
+        (bool success, bytes memory data) = addTaxa.call(abi.encodeWithSignature("setTaxaBase(uint256)", _val));
+        if(!success) {
+            revert("function call has failed");
+        }
+        return (data);
+    }
+    
+    function getTaxaViaCall() public returns (uint256) {
+        (bool success, bytes memory data) = addTaxa.call(abi.encodeWithSignature("taxaBase()"));
+        require(success, "function call has failed");        
+        return abi.decode(data, (uint256));        
+    }    
+}

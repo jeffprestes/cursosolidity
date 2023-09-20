ea4ee5ce512f8272037e91be20694f563378a436
Jeff Prestes <jeffprestes@gmail.com>
2023-09-20 16:42:32 -0300

    Update emprestimo-interface.sol

--- emprestimo-interface.sol
+++ emprestimo-interface.sol
@@ -1,1 +1,16 @@
-sss
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "contracts/itaxa.sol";
+
+contract EmprestimoComInterface {
+    ITaxa taxa;
+
+    constructor() {
+        taxa = ITaxa(0xEed2fE736e53E65412A3c3b8326260218bD40c16);
+    }
+
+    function simulaEmprestimo(uint256 valor, uint256 risco) external view returns (uint256) {
+        return valor*risco*taxa.taxaBase();
+    }    
+}

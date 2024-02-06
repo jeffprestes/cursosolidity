e8200720a95d1621c231e4742a95d927fd9f483e
Jeff Prestes <jeffprestes@gmail.com>
2024-02-06 19:16:40 -0300

    fix: ajusta require

--- nuclea-aula0602.sol
+++ nuclea-aula0602.sol
@@ -16,7 +16,7 @@ contract Faucet {
     // @dev incrementa um no acumulador e atribuir o valor do acumulador a um endereco ethereum.
     // @return valor atual do acumulador
     function atribuirValor() public returns (uint256) {
-        require(atribuicoes[msg.sender] > 0, "Sinto muito. Voce ja teve sua chance");
+        require(atribuicoes[msg.sender]==0, "Sinto muito. Voce ja teve sua chance");
         require(valorASerAtribuido < 256, "Sinto muito. Voce perdeu sua chance");
         valorASerAtribuido++;
         atribuicoes[msg.sender] = valorASerAtribuido;

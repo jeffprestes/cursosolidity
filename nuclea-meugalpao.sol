28847c286f0e1573875480d6d44173097be1af14
Jeff Prestes <jeffprestes@gmail.com>
2024-02-20 21:00:09 -0300

    aula de token 1155

---
+++ nuclea-meugalpao.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+
+contract MeuGalpao is ERC1155, Ownable, ERC1155Supply {
+    constructor(address initialOwner)
+        ERC1155("https://jeffprestes.github.io/meugalpao/")
+        Ownable(initialOwner)
+    {}
+
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
+
+    function mint(address account, uint256 id, uint256 amount, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mint(account, id, amount, data);
+    }
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mintBatch(to, ids, amounts, data);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address from, address to, uint256[] memory ids, uint256[] memory values)
+        internal
+        override(ERC1155, ERC1155Supply)
+    {
+        super._update(from, to, ids, values);
+    }
+
+    function tokenURI(uint256 id) public view virtual returns (string memory) {
+        return string(abi.encodePacked(uri(id), id, ".json"));
+    }
+}
